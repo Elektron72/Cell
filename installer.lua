7b8df67625e95563cf285df202c232f75767a2cc
Elektron72 <36974021+Elektron72@users.noreply.github.com>
2018-09-30 14:09:56 -0400

    Added installer

---
+++ installer.lua
@@ -0,0 +1,9 @@
+local shell = require("shell")
+local term = require("term")
+term.write("\nInstall Path: ")
+local installPath = string.gsub(term.read(), "\n", "") .. /
+shell.execute("wget https://raw.githubusercontent.com/Elektron72/Cell/master/cd_cell.lua " .. installPath .. "cd_cell.lua")
+shell.execute("wget https://raw.githubusercontent.com/Elektron72/Cell/master/cell.lua " .. installPath .. "cell.lua")
+shell.execute("wget https://raw.githubusercontent.com/Elektron72/Cell/master/delete_cell.lua " .. installPath .. "delete_cell.lua")
+shell.execute("wget https://raw.githubusercontent.com/Elektron72/Cell/master/programs.cfg " .. installPath .. "programs.cfg")
+print("Done!")
